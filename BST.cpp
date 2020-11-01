41d38007501ca60e2847104b931a726b71b6a1f1
ParzivalStark <luisanchez859@gmail.com>
2020-11-01 17:32:06 -0600

    Implementacion de inorder

--- BST.cpp
+++ BST.cpp
@@ -4,3 +4,19 @@ BST::BST(string fileName)
 {
 
 }
+
+void BST::inorder(int amount) {
+    inorder(amount, this->root);
+    cout << endl;
+}
+
+void BST::inorder(int &amount, NodeBST *current) {
+    if(current != nullptr) {
+        inorder(amount, current->right);
+        if (amount > 0) {
+            cout << "IP: " << current->ip << " Accesos: " << current->cont << endl;
+            amount--;
+        }
+        inorder(amount, current->left);
+    }
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,5 @@
 /*/
-Act 2.3 - Actividad Integral estructura de datos lineales
-Programa para el ordenamiento y busqueda en un
-registro de accesos ilegales a un servidor almacenados
-en el archivo "bitacora.txt"
+Act 3.4
 
 Owen Jauregui Borbon - A01638122
 Luis Humberto Sanchez Vaca - A01638029
