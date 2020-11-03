24af8a394b64d1753d77e414d5d3b5d6e333cb12
samueldiaz1004 <71342016+samueldiaz1004@users.noreply.github.com>
2020-11-02 20:02:32 -0600

    Documentacion de insert y remove

--- BST.cpp
+++ BST.cpp
@@ -1,7 +1,7 @@
 #include "BST.h"
 
-// Constructor de la clase BST, se encarga de leer las ips que 
-// se encuentran en el archivo con los accesos ordenados y 
+// Constructor de la clase BST, se encarga de leer las ips que
+// se encuentran en el archivo con los accesos ordenados y
 // registrar la cantidad de apariciones (sin considerar el puerto)
 // Recibe el nombre del archivo que contiene los accesos ordenados
 // Complejidad: O(n)
@@ -11,7 +11,7 @@ BST::BST(string fileName)
     this->root = nullptr;
 
     // Variables auxiliares para la insersion de nodos
-    string ip_, 
+    string ip_,
            pastIp = "";
     int ipReps = 0;
 
@@ -66,37 +66,62 @@ BST::BST(string fileName)
     }
 }
 
+// Destructor de la clase BST
+// Complejidad O(n)
 BST::~BST()
 {
-    removePostorder(root);
+    // Llamar a metodo para borrar todos los nodos del arbol
+    removePostorder(this->root);
 }
 
+// Metodo iterativo que coloca un nodo manteniendo las propiedades del BST
+// Recibe el numero de la ip (sin puerto) en formato string y
+// las veces que se repite en la bitacora
+// No regresa algun dato
+// Complejidad:
+//      Mejor de los casos O(1)
+//      Peor de los casos O(n)
+//      Promedio O(log(n))
 void BST::insert(string ip, int n)
 {
+    // Se crea un nuevo nodo con la informacion que se
+    // desea colocar en el BST
     NodeBST* nvo = new NodeBST(ip, n);
-
+    // Caso 1:
+    // El BST esta vacio
     if(this->root == nullptr){
+        // Se asigna a root el nuevo nodo
         this->root = nvo;
+        // termina la operacion
         return;
     }
-
+    // Nodos auxiliares para insertar nuevo nodo
     NodeBST* current = this->root;
     NodeBST* previous;
-
+    // Caso 2:
+    // Recorrer el BST hasta una hoja adecuada
     while(current != nullptr){
+        // Si el nodo es menor al que se esta comparando
+        // entonces current se desplaza a la izquierda del nodo al que apunta
         if(n < current->cont){
             previous = current;
             current = current->left;
         }
+        // Si el nodo es mayor al que se esta comparando
+        // o su valor es igual pero la ip es mayor,
+        // entonces current se desplaza a la derecha del nodo al que apunta
         else{
             previous = current;
             current = current->right;
         }
     }
-
+    // Si es menor, colocar el nuevo nodo a la izquierda
+    // de la hoja a la que esta apuntando
     if(n < previous->cont){
         previous->left = nvo;
     }
+    // De cualquier otra forma, colocar el nuevo nodo a
+    // la derecha de la hoja a la que esta apuntando
     else{
         previous->right = nvo;
     }
@@ -134,8 +159,14 @@ void BST::inorder(int &amount, NodeBST *current) {
     }
 }
 
+// Metodo recursivo que elimina todos los nodos del BST
+// Recibe apuntador a un nodo, empezando por la raiz
+// No regresa algun dato
+// Complejidad: O(n)
 void BST::removePostorder(NodeBST* current)
 {
+    // Sigue la estructura de un recorrido postorder
+    // para eliminar los nodos de las hojas hacia la raiz
     if(current != nullptr){
         removePostorder(current->left);
         removePostorder(current->right);
